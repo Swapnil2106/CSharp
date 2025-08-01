c2bf3884ab754238ffa802bf0663b5334416ba24
Swapnil <swapnillkore@gmail.com>
2025-08-01 05:48:53 +0530

    Add project files.

---
+++ ConsoleApps/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. This is how you print Hello World in C# on the console.
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
