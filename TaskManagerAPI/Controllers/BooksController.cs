10943532a4afb53c6764b4aac0982631a2b2b2ea
Swapnil <swapnillkore@gmail.com>
2025-08-07 10:32:20 +0530

    Seeded data into DB manually and verified it through Swagger api's

---
+++ TaskManagerAPI/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagerAPI.Data;
+
+namespace TaskManagerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BooksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            var books = _context.Books.ToList();
+            return Ok(books);
+        }
+    }
+}
---
+++ TaskManagerAPI/DBScripts/SQLQuery1.sql
@@ -0,0 +1,19 @@
+--Dummy data inserted in Books table
+--INSERT INTO Books (Title, Author, IsAvailable) VALUES
+--('Atomic Habits', 'James Clear', 1),
+--('Deep Work', 'Cal Newport', 1),
+--('The Pragmatic Programmer', 'Andrew Hunt', 1),
+--('Clean Code', 'Robert C. Martin', 1),
+--('C# in Depth', 'Jon Skeet', 1),
+--('You Don’t Know JS', 'Kyle Simpson', 1),
+--('The Clean Coder', 'Robert C. Martin', 0),
+--('Design Patterns', 'Erich Gamma', 1),
+--('The Art of Computer Programming', 'Donald Knuth', 1),
+--('Cracking the Coding Interview', 'Gayle Laakmann McDowell', 0),
+--('Introduction to Algorithms', 'Thomas H. Cormen', 1),
+--('Refactoring', 'Martin Fowler', 1),
+--('Head First Design Patterns', 'Eric Freeman', 1),
+--('Zero to One', 'Peter Thiel', 1),
+--('The Mythical Man-Month', 'Fred Brooks', 0);
+
+select * from Books with(nolock);
